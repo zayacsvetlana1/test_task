742ebcda789e46a3fe759dd8920f3495f839703d
EvaCzvetaeva <czvetaeva.87@bk.ru>
2022-04-26 21:56:33 +0300

    feat: init userAPI

--- components/UserList.js
+++ components/UserList.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import { useSelector} from "react-redux";
 import {fetchUsers} from "../store/action-creators/user";
 import {useActions} from "../hooks/useActions";
 
