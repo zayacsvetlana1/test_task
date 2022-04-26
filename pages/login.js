b5b7acce60af628ad16a7d4ea7ec44163fa25469
EvaCzvetaeva <czvetaeva.87@bk.ru>
2022-04-26 21:03:05 +0300

    feat: init loginForm

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,25 @@
+import {Button, Card, Container, Form} from "react-bootstrap";
 
 
 export default function Login () {
 	return (
-		<div>
-			Login
-		</div>
+		<Container className='d-flex justify-content-center'>
+			<Card style={{width: 600}} className='p-5 mt-4'>
+				<Form>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>E-mail</Form.Label>
+						<Form.Control type="email" placeholder='Введите ваш e-mail...' />
+					</Form.Group>
+
+					<Form.Group className="mb-3" controlId="formBasicPassword">
+						<Form.Label>Пароль</Form.Label>
+						<Form.Control type="password" placeholder='Введите ваш пароль...' />
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Войти
+					</Button>
+				</Form>
+			</Card>
+		</Container>
 	)
 }
